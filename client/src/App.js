142764d9bf76ed9699081f5df90c1bffcf9ff2c5
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-10-11 19:46:27 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import logo from './images/Site-logo.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import useStyles from './styles'
 
 const App = () => {
+    const classes = useStyles();
     return(
-        <>
-        <div>
-            <h1>hi</h1>
-        </div>
-        </>
+        <Container maxWidth="lg">
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
