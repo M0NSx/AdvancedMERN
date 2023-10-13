83e869619e399af81f08ca92a00377c570c79465
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-10-13 16:29:01 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
-import logo from './images/Site-logo.png'
-import Posts from './components/Posts/Posts'
-import Form from './components/Form/Form'
-import useStyles from './styles'
+import { useDispatch } from "react-redux";
+
+import { getPosts } from "./actions/posts";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import logo from './images/Site-logo.png';
+import makeStyles from './styles';
 
 const App = () => {
-    const classes = useStyles();
+    const classes = makeStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+    
     return(
-        <Container maxWidth="lg">
+        <Container maxWidth='lg'>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Travel's</Typography>
+                <img className={classes.image} src={logo} alt="travel" height="60" />
+            </AppBar>
             <Grow in>
                 <Container>
                     <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
