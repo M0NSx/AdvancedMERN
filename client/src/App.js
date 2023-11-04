81d565103abfcb3b92e5747ec892ba48f4f8da92
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-11-03 23:43:27 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import { useDispatch } from "react-redux";
 
